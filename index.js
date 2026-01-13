b8ad20cd99ac22c845abb80396ae22799e4972b2
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-14 01:09:29 +0300

    added new MVC

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from "express";
 const app = express();
+import dotenv from 'dotenv'
+const PORT = process.env.PORT || 2000
+import mongoose from "mongoose";
+dotenv.config()
+import getUser from './routes/user.js'
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("hello world");
 });
+app.use('/create', getUser )
 
-app.listen(2000, () => {
-  console.log("server runing:http://localhost:6000");
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> console.log("✅ Connected Locally"))
+    .catch((err)=> console.log("❌ Error Connection", err))
+
+app.listen(PORT, () => {
+  console.log(`server runing:http://localhost:${PORT}`);
 });
