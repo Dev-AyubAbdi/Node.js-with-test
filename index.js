063baf09048c532eb6f20459413bf601d11eeb07
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-14 00:47:19 +0300

    added gitignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from 'express'
-const app = express()
-app.use(express.json())
-app.get('/test', (req, res) => {
-    res.json("hello world")
-})
+import express from "express";
+const app = express();
 
+app.use(express.json());
 
-app.lis
+app.get("/", (req, res) => {
+  res.json("hello world");
+});
 
+app.listen(2000, () => {
+  console.log("server runing:http://localhost:6000");
+});
