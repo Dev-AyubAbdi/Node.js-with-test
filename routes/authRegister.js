83085ac9d3d956e688873c46b5f5093359a1222d
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-15 10:25:45 +0300

    auth protect middlewares

--- routes/authRegister.js
+++ routes/authRegister.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { login, register } from '../controllers/authRegister.js'
+import { protect } from '../middlewares/auth.js'
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+
+router.get('/profile', protect, (req, res) => {
+  res.json(req.user)
+})
 
 
 export default router
